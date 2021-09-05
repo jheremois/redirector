288f39c7fd75f73bc40e541489e23e833efce824
jheremy <jheremy808@gmail.com>
2021-09-05 11:07:20 -0400

    Reporte

--- index.js
+++ index.js
@@ -7,11 +7,18 @@ const link = 'https://github.com/jheremois'
 app.use(
     (req, res)=>{
         res.redirect(link)
+    },
+    ()=>{
+        console.log('?');
     }
 )
 app.get('/*', (req, res)=>{
     res.redirect(link)
+}, ()=>{
+    console.log('!');
 })
 
 
-app.listen(process.env.PORT || 4000)
+app.listen(process.env.PORT || 4000, ()=>{
+    console.log('En linea en el puerto 4000');
+})
